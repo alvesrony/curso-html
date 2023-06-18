8a4bb3b4d62591a4a9ae16fdf14d2f7fdabd6833
Your Name <alvesroniesley@gmail.com>
2023-06-17 22:39:24 -0300

    adicionando conteudo das aulas

---
+++ javascript/meu_script.js
@@ -0,0 +1,8 @@
+// alert('Olá, seja bem vindo ao curso!!!')
+
+/*
+	selecionar esse elemento no DOM
+	atualizar o valor desse elemento com uma string
+*/
+
+document.getElementById('nome').value = 'oi'
